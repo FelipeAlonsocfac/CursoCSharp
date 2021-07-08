dbc2091fe99b5e38ae6b0729f6c7e628d6400dcd
NiicolasDiiaz <nicolasfelipediaz@gmail.com>
2021-07-08 18:38:50 -0500

    Punto 2 completo

--- Nicolas/ConsoleApp1/Seccion5/Program.cs
+++ Nicolas/ConsoleApp1/Seccion5/Program.cs
@@ -65,7 +65,60 @@ namespace Seccion5
         }
         public static void Punto2()
         {
+            int numeroSelectorCalculadora = MenuCalculadora();
+            Console.WriteLine("Ingrese primer numero:");
+            double primerNumero = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese segundo numero:");
+            double segundoNumero = double.Parse(Console.ReadLine());
 
+            switch (numeroSelectorCalculadora)
+            {
+                case 1:
+                    Console.WriteLine("El resultado es: " + Suma(primerNumero, segundoNumero));
+                    break;
+
+                case 2:
+                    Console.WriteLine("El resultado es: " + Resta(primerNumero, segundoNumero));
+                    break;
+
+                case 3:
+                    Console.WriteLine("El resultado es: " + Multiplicacion(primerNumero, segundoNumero));
+                    break;
+
+                case 4:
+                    Console.WriteLine("El resultado es: " + Division(primerNumero, segundoNumero));
+                    break;
+
+                default:
+                    break;
+            }
+
+            
+            static int MenuCalculadora()
+            {
+                Console.WriteLine("1- Suma");
+                Console.WriteLine("2- Resta");
+                Console.WriteLine("3- Multiplicacion");
+                Console.WriteLine("4- Division");
+
+                return int.Parse(Console.ReadLine());
+            }
+            static double Suma(double numero1, double numero2)
+            {
+                return (numero1+numero2);
+            }
+            static double Resta(double numero1, double numero2)
+            {
+                return (numero1 - numero2);
+            }
+            static double Multiplicacion(double numero1, double numero2)
+            {
+                return (numero1 * numero2);
+            }
+            static double Division(double numero1, double numero2)
+            {
+                return (numero1 / numero2);
+            }
         }
         public static void Punto3()
         {
