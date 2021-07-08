5317f2d21a762256a2c295f6bb04981323411d55
NiicolasDiiaz <nicolasfelipediaz@gmail.com>
2021-07-08 17:59:54 -0500

    Menu total

--- Nicolas/ConsoleApp1/Seccion5/Program.cs
+++ Nicolas/ConsoleApp1/Seccion5/Program.cs
@@ -6,7 +6,74 @@ namespace Seccion5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int numeroMenu = Menu();
+            while (numeroMenu >= 1 && numeroMenu <= 6)
+            {
+                SelectorMenu(ref numeroMenu);
+            }
+        }
+
+        public static int Menu()
+        {
+            Console.Write("Ingrese el punto deseado (1-6)");
+            return int.Parse(Console.ReadLine());
+        }
+        public static void SelectorMenu(ref int numeroMenu)
+        {
+            switch (numeroMenu)
+            {
+                case 1:
+                    Punto1();
+                    break;
+
+                case 2:
+                    Punto2();
+                    break;
+
+                case 3:
+                    Punto3();
+                    break;
+
+                case 4:
+                    Punto4();
+                    break;
+
+                case 5:
+                    Punto5();
+                    break;
+
+                case 6:
+                    Punto6();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        public static void Punto1()
+        {
+
+        }
+        public static void Punto2()
+        {
+
+        }
+        public static void Punto3()
+        {
+
+        }
+        public static void Punto4()
+        {
+
+        }
+        public static void Punto5()
+        {
+
+        }
+        public static void Punto6()
+        {
+
         }
     }
 }
