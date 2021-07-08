38dbed2ffa14e0bef59046e9bc25ff5483677739
NiicolasDiiaz <nicolasfelipediaz@gmail.com>
2021-07-08 11:55:55 -0500

    Primer commit ejercicios seccion 4

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Punto 1
+
+            var variableUno = 1;
+            var variableDos = 2;
+            var variableTres = 3;
+
+            var Suma = variableUno + variableDos + variableTres;
+            Console.WriteLine(Suma);
+
+            #endregion
+
+            #region Punto 2
+
+            int numUno = int.Parse(Console.ReadLine());
+            int numDos = int.Parse(Console.ReadLine());
+
+            if (numUno >= numDos)
+            {
+                Console.WriteLine(numUno);
+            }
+            else
+            {
+                Console.WriteLine(numDos);
+            }
+
+            #endregion
+
+            #region Punto 3
+
+            string dia = 
+
+            #endregion
+
+        }
+    }
+}
