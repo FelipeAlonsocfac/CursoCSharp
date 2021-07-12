4054a1cae5c5b2976dbfaaa2257044313b13c6be
FelipeAlonso <xfelipe98@gmail.com>
2021-07-12 11:57:17 -0500

    Ejercicios seccion 10

---
+++ FELIPE/EjerciciosSeccion10,Interfaces/EjerciciosSeccion10/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosSeccion10
+{
+    class Animal : IAnimal
+    {
+        public void Andar()
+        {
+            Console.WriteLine("Andando...");
+        }
+
+        public bool EsPerro()
+        {
+            return true;
+        }
+
+        public void Saltar()
+        {
+            Console.WriteLine("Saltando...");
+        }
+    }
+}
---
+++ FELIPE/EjerciciosSeccion10,Interfaces/EjerciciosSeccion10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EjerciciosSeccion10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+
+            animal.Andar();
+            animal.Saltar();
+            Console.WriteLine($"\nEs perro?: {animal.EsPerro()}");
+        }
+    }
+}
