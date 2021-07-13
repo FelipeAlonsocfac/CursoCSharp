7c8b55d428effda4a0fe8c38b20b8e4c070a3482
FelipeAlonso <xfelipe98@gmail.com>
2021-07-13 13:40:49 -0500

    Añadi seccion 13, manejo de archivos

---
+++ FELIPE/EjerciciosSeccion13,ArchivosStreams/EjerciciosStreams/ManejoFilestreams.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosStreams
+{
+    class ManejoFilestreams
+    {
+        FileStream mainFs;
+
+        public ManejoFilestreams() {
+            FileStream inicializacion = new FileStream("Personas.txt", FileMode.OpenOrCreate);
+            inicializacion.Close();
+        }
+        public Persona Add() {
+
+            Persona personaActual = new Persona();
+
+            string otraPersona;
+
+            mainFs = new FileStream("Personas.txt", FileMode.Append);
+
+            while (true)    //se queda hasta que haya un break, este lo hay cuando ya no se quieren añadir a mas personas
+            {
+
+                Console.WriteLine("Introduzca nombre de la persona a registrar\n");
+                personaActual.nombre = Console.ReadLine();
+
+                Console.WriteLine("Introduzca edad de la persona a registrar\n");
+
+
+                int result; //resultado de un parse exitoso
+
+                while (!int.TryParse(Console.ReadLine(), out result))  //si no funciona el parse, reintentarlo
+                {
+                    Console.WriteLine("Valor incorrecto");
+                    Console.WriteLine("Introduzca edad de la persona a registrar\n");
+                }
+
+                personaActual.edad = result;
+
+                Console.WriteLine("Introduzca localidad de la persona a registrar");
+                personaActual.localidad = Console.ReadLine();
+
+                string persona = $"{personaActual.nombre}|{personaActual.edad}|{personaActual.localidad};";
+
+                mainFs.Write(ASCIIEncoding.ASCII.GetBytes(persona), 0, persona.Length);
+
+                while (true)    //while para asegurarse que recibe S o N
+                {
+                    Console.WriteLine("Quieres introducir otra persona?(S/N)");
+                    otraPersona = Console.ReadLine().ToUpper();
+                    if (otraPersona == "S" || otraPersona == "N") break;
+                    Console.WriteLine("Valor incorrecto.\n");
+                }
+                if (otraPersona == "N") //si no se quiere añadir otra persona, que salga del while
+                {
+                    break;
+                }
+            }
+            mainFs.Close();
+            return personaActual;
+        }
+
+        public List<Persona> Get() {
+            byte[] infoArchivo = new byte[50000];
+            List<Persona> listaPersonas = new List<Persona>();
+
+            mainFs = new FileStream("Personas.txt", FileMode.Open);
+            mainFs.Read(infoArchivo, 0, (int)mainFs.Length);
+            mainFs.Close();
+
+            var archivo = ASCIIEncoding.ASCII.GetString(infoArchivo);
+
+            var personas = archivo.Split(';').ToList();
+
+            personas.RemoveAt(personas.Count - 1);
+
+            foreach (var item in personas)
+            {
+                listaPersonas.Add(ExtractPersona(item));
+            }
+            return listaPersonas;
+        }
+        private Persona ExtractPersona(string linea)
+        {
+
+            var persona = linea.Split('|');
+
+            return new Persona
+            {
+                nombre = persona[0],
+                edad = int.Parse(persona[1]),
+                localidad = persona[2]
+                
+            };
+        }
+
+    }
+}
---
+++ FELIPE/EjerciciosSeccion13,ArchivosStreams/EjerciciosStreams/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace EjerciciosStreams
+{
+    class Program
+    {
+        private static List<Persona> personasArchivo = new List<Persona>();
+        private static ManejoFilestreams fsPersonas = new ManejoFilestreams();
+        private static Persona personaActual = new Persona();
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                SwitchMenu();
+            }
+        }
+        public static string Menu()
+        {
+            Console.WriteLine("*****Menu*****");
+            Console.WriteLine("*****1. Nueva persona*****");
+            Console.WriteLine("*****2. Ver personas*****");
+            Console.WriteLine("*****3. Salir*****");
+            return Console.ReadLine();
+        }
+        public static void SwitchMenu()
+        {
+            switch (Menu())
+            {
+                case "1":
+                    personaActual = fsPersonas.Add();
+                    break;
+                case "2":
+                    personasArchivo = fsPersonas.Get();
+                    foreach (var item in personasArchivo)
+                    {
+                        Console.WriteLine($"Nombre: {item.nombre}. Edad: {item.edad}. Localidad: {item.localidad}");
+                    }
+                    break;
+                case "3":
+                    System.Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Opcion invalida");
+                    break;
+            }
+        }
+    }
+}
